cbbe5f4b7c12abd7d852043a2379e60a3ae68d05
Joshua Libradilla <joshua.libradilla@usjr.edu.ph>
2022-10-15 09:54:34 +0800

    Project Initialization

---
+++ src/main/java/ph/com/alliance/prelims/g4/ticket/TicketApplication.java
@@ -0,0 +1,13 @@
+package ph.com.alliance.prelims.g4.ticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketApplication.class, args);
+	}
+
+}
