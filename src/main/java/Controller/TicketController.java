1165011cbb754af92e750d9845e8093f0a024e83
Kent Telebanco <ktelebanco@gmail.com>
2022-10-15 10:36:35 +0800

    Ticket Controller Initializer

--- src/main/java/Controller/TicketController.java
+++ src/main/java/Controller/TicketController.java
@@ -1,5 +1,19 @@
 package Controller;
 
-public class TicketController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import Service.TicketService;
+
+
+@RestControllerAdvice
+public class TicketController {
+	
+	private TicketService service ;
+	
+@Autowired
+	public TicketService ticketController (final TicketService service) {
+	return this.service = service;
+	}
+	
 }
