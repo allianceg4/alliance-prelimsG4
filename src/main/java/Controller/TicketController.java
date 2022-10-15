02f5ced8e8b1d83cd715b2b8ef0d2bf27204644b
Kent Telebanco <ktelebanco@gmail.com>
2022-10-15 11:51:59 +0800

    Updated Ticket Controller

--- src/main/java/Controller/TicketController.java
+++ src/main/java/Controller/TicketController.java
@@ -1,13 +1,19 @@
 package Controller;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import Entity.Ticket;
 import Service.TicketService;
 
 @RestController
@@ -20,19 +26,34 @@ public class TicketController {
 		return this.service=service;
 	}
 	
+	//gets the ticket id
 	public String getTicketId(@PathVariable final int id) {
-		return "";
+		return service.findById(id);
 	}
 	
-	public String getAll() {
-		return "";
+	//gets all the ticket id
+	public String getAllTicket() {
+		return service.findAll();
 	}
 	
-	public int save (final HttpServletRequest request) throws IOException{
-		return 0;
+	//post map and save ticket
+	@PostMapping("/user/{id}")
+	public int saveTicket (final HttpServletRequest request) throws IOException{
+		final BufferedReader body = request.getReader();
+		return service.createTicket(body);
 	}
 	
-	public int delete (@PathVariable final int id) throws IOException{
-		return 0;
+	//post map and update ticket assignee
+	@PostMapping("/user/{id}")
+	public int save(@PathVariable final int id, @RequestParam("ticket") final Ticket ticket, @RequestParam("assginee") final String assignee) throws IOException {
+		return service.updateTicketAssignee(ticket, assignee);
 	}
-}
+	
+	//deletes element
+	@DeleteMapping("/user/delete/{id}")
+	public int deleteTicket (@PathVariable final int id) throws IOException{
+		return service.deleteTicket(id);
+	}
+	
+	}
+	
