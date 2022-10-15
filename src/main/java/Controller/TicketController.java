2a65909f01f00eecd515a21cf77f0c1fdd9e9eaf
Kent Telebanco <ktelebanco@gmail.com>
2022-10-15 10:57:41 +0800

    Updated Ticket Controller

--- src/main/java/Controller/TicketController.java
+++ src/main/java/Controller/TicketController.java
@@ -1,5 +1,38 @@
 package Controller;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import Service.TicketService;
+
+@RestController
 public class TicketController {
 
+	private TicketService service;
+	
+	@Autowired
+	public TicketService TicketController (final TicketService service) {
+		return this.service=service;
+	}
+	
+	public String getTicketId(@PathVariable final int id) {
+		return "";
+	}
+	
+	public String getAll() {
+		return "";
+	}
+	
+	public int save (final HttpServletRequest request) throws IOException{
+		return 0;
+	}
+	
+	public int delete (@PathVariable final int id) throws IOException{
+		return 0;
+	}
 }
